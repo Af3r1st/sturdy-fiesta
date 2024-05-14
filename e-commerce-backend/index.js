b54ebf2c52a7d35ac3ba639fcd16ba56f7d49fa6
Af3r1st <150523476+Af3r1st@users.noreply.github.com>
2024-05-14 11:55:13 +0200

    Update index.js

--- e-commerce-backend/index.js
+++ e-commerce-backend/index.js
@@ -6,18 +6,15 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
-const { type } = require("os");
-const cookieParser = require('cookie-parser');
 
 app.use(express.json());
 app.use(cors());
 
 // Database Connection With MongoDB
-
 mongoose.connect("mongodb+srv://levurda2003:123123123@cluster0.takr61a.mongodb.net/e-commerce");
 
 
-
+//Image Storage Engine 
 const storage = multer.diskStorage({
     destination: './upload/images',
     filename: (req, file, cb) => {
@@ -38,21 +35,14 @@ app.use('/images', express.static('upload/images'));
 const fetchuser = async (req, res, next) => {
   const token = req.header("auth-token");
   if (!token) {
-      return res.status(401).send({ errors: "Please authenticate using a valid token" });
+    res.status(401).send({ errors: "Please authenticate using a valid token" });
   }
   try {
-      const data = jwt.verify(token, "secret_ecom");
-      req.user = data.user;
-
-      // Check if the user is an admin
-      const user = await Users.findById(req.user.id);
-      if (!user || user.role !== "admin") {
-          return res.status(403).send({ errors: "Access denied. You must be an admin." });
-      }
-
-      next();
+    const data = jwt.verify(token, "secret_ecom");
+    req.user = data.user;
+    next();
   } catch (error) {
-      return res.status(401).send({ errors: "Please authenticate using a valid token" });
+    res.status(401).send({ errors: "Please authenticate using a valid token" });
   }
 };
 
@@ -75,11 +65,6 @@ const Users = mongoose.model("Users", {
   date: {
     type: Date,
     default: Date.now,
-  },
-  role: {
-    type: String,
-    enum: ["user", "admin"], 
-    default: "user", 
   },
 });
 
@@ -147,100 +132,36 @@ app.post('/login', async (req, res) => {
         return res.status(400).json({success: success, errors: "please try with correct email/password"})
     }
 })
-//
-const getCheckToken = async (req, res, next) => {
-
-  try {
-    const token = req.cookies.token;
-    const decodedToken = await jwt.verify(token, 'secret');
-    const user = await decodedToken;
-    const userId = user.id;
-    const findedUser = await Users.findById(userId);
-    if(!findedUser){
-      return res.status(404).json({ msg: "User nebyl nalezen." });
-    }
-
-    else{
-      req.user = user;
-      next();
-    }
-} catch (err) {
-    return res.status(401).json({ message: "Invalid token, please login" });
-}
-};
-//
-
-app.get("/get-checktoken", getCheckToken, (req, res) => {
-    return res.json({
-        message: "You are authenticated"
-    }) 
-});
-
-
-
 
 //Create an endpoint at ip/auth for regestring the user in data base & sending token
 app.post('/signup', async (req, res) => {
   console.log("Sign Up");
-  let success = false;
-  let check = await Users.findOne({ email: req.body.email });
-  if (check) {
-      return res.status(400).json({ success: success, errors: "existing user found with this email" });
-  }
-  let cart = {};
-  for (let i = 0; i < 300; i++) {
-      cart[i] = 0;
-  }
-  const user = new Users({
-      name: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-      cartData: cart,
-      role: req.body.role || "user", 
-  });
-  await user.save();
-  const data = {
-      user: {
-          id: user.id
-      }
-  };
-
-  //Creating Admin acc
-  app.post('/admin/signup', async (req, res) => {
-    console.log("Admin Sign Up");
-    
-    let success = false;
-    let check = await Users.findOne({ email: req.body.email });
-    if (check) {
-        return res.status(400).json({ success: success, errors: "existing user found with this email" });
-    }
-    let cart = {};
-    for (let i = 0; i < 300; i++) {
-        cart[i] = 0;
-    }
-    const admin = new Users({
-        name: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-        cartData: cart,
-        role: "admin", 
-    });
-    await admin.save();
-    const data = {
-        user: {
-            id: admin.id
+        let success = false;
+        let check = await Users.findOne({ email: req.body.email });
+        if (check) {
+            return res.status(400).json({ success: success, errors: "existing user found with this email" });
         }
-    };
-
-    const token = jwt.sign(data, 'secret_ecom');
-    success = true;
-    res.json({ success, token })
-});
-
-  const token = jwt.sign(data, 'secret_ecom');
-  success = true;
-  res.json({ success, token })
-});
+        let cart = {};
+          for (let i = 0; i < 300; i++) {
+          cart[i] = 0;
+        }
+        const user = new Users({
+            name: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            cartData: cart,
+        });
+        await user.save();
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        
+        const token = jwt.sign(data, 'secret_ecom');
+        success = true; 
+        res.json({ success, token })
+    })
 
 app.get("/allproducts", async (req, res) => {
 	let products = await Product.find({});
@@ -264,23 +185,14 @@ app.get("/popularinwomen", async (req, res) => {
 
 //Create an endpoint for saving the product in cart
 app.post('/addtocart', fetchuser, async (req, res) => {
-  console.log("Add Cart");
-  try {
-      const userData = await Users.findOne({ _id: req.user.id });
-      if (!userData) {
-          return res.status(404).json({ errors: "User not found" });
-      }
-      // Assuming req.body.itemId contains the ID of the product to be added to the cart
-      userData.cartData[req.body.itemId] += 1;
-      await userData.save();
-      return res.send("Added to cart successfully");
-  } catch (error) {
-      console.error("Error adding product to cart:", error);
-      return res.status(500).send({ errors: "Internal server error" });
-  }
-});
+	console.log("Add Cart");
+    let userData = await Users.findOne({_id:req.user.id});
+    userData.cartData[req.body.itemId] += 1;
+    await Users.findOneAndUpdate({_id:req.user.id}, {cartData:userData.cartData});
+    res.send("Added")
+  })
 
-  //Create an endpoint for removing the product in cart
+  //Create an endpoint for saving the product in cart
 app.post('/removefromcart', fetchuser, async (req, res) => {
 	console.log("Remove Cart");
     let userData = await Users.findOne({_id:req.user.id});
@@ -292,7 +204,7 @@ app.post('/removefromcart', fetchuser, async (req, res) => {
     res.send("Removed");
   })
 
-  //Create an endpoint for geting the product in cart
+  //Create an endpoint for saving the product in cart
 app.post('/getcart', fetchuser, async (req, res) => {
   console.log("Get Cart");
   let userData = await Users.findOne({_id:req.user.id});
@@ -335,3 +247,4 @@ app.listen(port, (error) => {
   if (!error) console.log("Server Running on port " + port);
   else console.log("Error : ", error);
 });
+
